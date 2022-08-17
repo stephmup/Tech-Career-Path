0ad4b1e99bde4d74efd804f5dba8e4587dd610a9
stephmup <stephmupofficial@icloud.com>
2022-08-17 19:49:32 +0200

    ..

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // navbar variables
 const nav = document.querySelector('.mobile-nav');
 const navMenuBtn = document.querySelector('.nav-menu-btn');
@@ -7,7 +5,7 @@ const navCloseBtn = document.querySelector('.nav-close-btn');
 
 
 // navToggle function
-const navToggleFunc = function () { nav.classList.toggle('active'); }
+const navToggleFunc = function() { nav.classList.toggle('active'); }
 
 navMenuBtn.addEventListener('click', navToggleFunc);
 navCloseBtn.addEventListener('click', navToggleFunc);
@@ -20,22 +18,22 @@ const themeBtn = document.querySelectorAll('.theme-btn');
 
 for (let i = 0; i < themeBtn.length; i++) {
 
-  themeBtn[i].addEventListener('click', function () {
+    themeBtn[i].addEventListener('click', function() {
 
-    // toggle `light-theme` & `dark-theme` class from `body`
-    // when clicked `theme-btn`
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
+        // toggle `light-theme` & `dark-theme` class from `body`
+        // when clicked `theme-btn`
+        document.body.classList.toggle('light-theme');
+        document.body.classList.toggle('dark-theme');
 
-    for (let i = 0; i < themeBtn.length; i++) {
+        for (let i = 0; i < themeBtn.length; i++) {
 
-      // When the `theme-btn` is clicked,
-      // it toggles classes between `light` & `dark` for all `theme-btn`.
-      themeBtn[i].classList.toggle('light');
-      themeBtn[i].classList.toggle('dark');
+            // When the `theme-btn` is clicked,
+            // it toggles classes between `light` & `dark` for all `theme-btn`.
+            themeBtn[i].classList.toggle('light');
+            themeBtn[i].classList.toggle('dark');
 
-    }
+        }
 
-  })
+    })
 
 }
